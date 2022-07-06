a716dbb1f5c4962880b5330eb74ffe3479cafd95
lpd0916 <90810762+Charles2001Dong@users.noreply.github.com>
2022-07-06 11:23:58 +0800

    feat: add LabelItem ViewMore Component

---
+++ src/components/ViewMore.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="md:py-1 py-0.5 md:px-4 px-2  border border-solid border-permaBorderGreen rounded-md bg-permaGreen8 text-permaWhite2 text-14px cursor-pointer">
+    {{ t('nft.view_more') }}
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+
+</script>
+
+<style>
+</style>
