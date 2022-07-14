8871828063d11ff6f5332f0643208ae14df50f11
lpd0916 <90810762+Charles2001Dong@users.noreply.github.com>
2022-07-14 18:12:50 +0800

    fix: update NftCardInfo & ViewMore  layout Style

--- src/components/NftCardInfo.vue
+++ src/components/NftCardInfo.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="md:mt-5 mt-10px">
+  <div class="md:mt-4 mt-10px md:px-1 px-0.5">
     <div class="flex justify-between text-permaWhite3 text-12px">
       <span>{{ t('nft.price') }}</span>
       <span>{{ t('nft.owned_by') }}</span>
     </div>
-    <div class="flex mt-0.5 justify-between md:text-base text-14px">
-      <div class="flex items-center">
-        <img :src="require(`@/images/${symbol}.png`)" class="mr-1 w-5 h-5">
-        <span>{{ amount }}</span>
+    <div class="flex md:mt-0.5 justify-between md:text-base text-14px">
+      <div v-if="amount" class="flex items-center">
+        <TokenLogo :symbol="symbol" class="mr-1 w-5 h-5" />
+        <span>{{ price }}</span>
       </div>
-      <div>
-        {{ formatAddress }}
+      <div v-else class="hover:text-permaGreen9 transition-all">
+        <a :href="permaLink" target="_blank">
+          Make offer
+        </a>
+      </div>
+      <div class="hover:text-permaGreen9 text-permaWhite2 transition-all">
+        <a :href="ownerLink" target="_blank">
+          {{ formatAddress }}
+        </a>
       </div>
     </div>
   </div>
@@ -19,13 +26,21 @@
 <script setup lang='ts'>
 import { computed, defineProps } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { toBN } from '@/lib/util'
+import TokenLogo from '@/components/TokenLogo.vue'
 interface Props {
   symbol: string
   amount: string
   address: string
+  ownerLink: string
+  permaLink: string
 }
+
 const props = defineProps<Props>()
 const { t } = useI18n()
+const price = computed(() => {
+  return toBN(props.amount).toFormat(2)
+})
 const formatAddress = computed(() => {
   return props.address.slice(0, 4) + '...' + props.address.slice(-2)
 })
--- src/components/ViewMore.vue
+++ src/components/ViewMore.vue
@@ -1,13 +1,23 @@
 <template>
-  <div
-    class="md:py-1 py-0.5 md:px-4 px-2  border border-solid border-permaBorderGreen rounded-md bg-permaGreen8 text-permaWhite2 text-14px cursor-pointer">
+  <Active class="md:py-1.5 py-1 md:px-4 px-2 border border-solid rounded-md text-14px cursor-pointer" :class-name="className" :default-class-name="defaultClassName">
     {{ t('nft.view_more') }}
-  </div>
+  </Active>
 </template>
 
 <script setup lang='ts'>
+import { defineProps, withDefaults } from 'vue'
+import Active from './Active.vue'
 import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
+interface Props {
+  className: string
+  defaultClassName: string
+}
+
+withDefaults(defineProps<Props>(), {
+  className: '',
+  defaultClassName: ''
+})
 
 </script>
 
