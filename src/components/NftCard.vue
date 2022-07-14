5c16d76dd5e5cf40f4deab43ab0faf39c79a655a
lpd0916 <90810762+Charles2001Dong@users.noreply.github.com>
2022-07-14 18:11:51 +0800

    fix: update BatchCard & batchItems & NftCard style

--- src/components/NftCard.vue
+++ src/components/NftCard.vue
@@ -1,27 +1,42 @@
 <template>
-  <div class="md:pt-3 md:pb-6 pb-10px md:px-3 pt-2 px-2 rounded-2xl bg-permaBlack md:w-282px w-164px sm:mb-20 mb-8">
-    <div style="border-bottom: 2px dashed rgba(255, 255, 255, 0.08);" class="md:pb-5 pb-10px">
-      <div class="rounded-lg overflow-hidden">
-        <img
-          src="https://lh3.googleusercontent.com/esFloHANgGEXjKz9ff2mLNVG6LKEjkcEjnhe1_fOQr7Yob0BjwW1kfNXEzbFlfq2sFcfBRJRPuljNvAYcly8Qshjuu-aY1LzH4h0KDs"
-          alt="">
-      </div>
-      <div class="md:mt-5 mt-10px">
-        <div class="text-14px md:text-base">
-          Musician
+  <div class="md:pt-3 md:pb-4 pb-10px md:px-3 pt-2 px-2 rounded-2xl bg-permaBlack md:w-282px w-164px">
+    <div class="md:pb-4 pb-10px border-b md:border-b-2 border-permaWhite5 border-dashed">
+      <a :href="permaLink" target="_blank">
+        <div class="rounded-lg overflow-hidden md:w-258px md:h-258px w-148px h-148px" :style="`background-image: url(${imageUrl});background-position:bottom;background-size:cover`" />
+      </a>
+      <div class="md:mt-4 mt-10px md:px-1 px-0.5">
+        <div class="text-14px md:text-base truncate ">
+          {{ name }}
         </div>
-        <div class="md:mt-0.5 text-12px md:text-14px text-permaWhite3">
-          Wonderland and the Funny Fellows
+        <div class="md:mt-0.5 text-12px md:text-14px text-permaWhite3 truncate">
+          {{ collectionName }}
         </div>
       </div>
     </div>
-    <CardInfo symbol="eth" amount="100" address="0xfc65E09Ef6674DdB4D8a6f3b6a6c8D9d55d67716" />
+    <NftCardInfo
+      :symbol="symbol"
+      :amount="amount"
+      :address="owner"
+      :perma-link="permaLink"
+      :owner-link="ownerLink" />
   </div>
 </template>
 
 <script setup lang='ts'>
-import CardInfo from './NftCardInfo.vue'
-
+import { defineProps } from 'vue'
+import NftCardInfo from './NftCardInfo.vue'
+interface Props {
+  imageUrl: string
+  owner: string
+  name: string
+  collectionName: string
+  permaLink: string
+  ownerLink: string
+  amount: string
+  symbol: string
+}
+const props = defineProps<Props>()
+console.log(props.amount)
 </script>
 
 <style>
