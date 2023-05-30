e83a509811baab9562ad4d6174e83a2d93ff7f81
lpd0916 <1985413702@qq.com>
2023-05-30 10:57:57 +0800

    chore: hidden permdao link & isMobile

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -86,3 +86,34 @@ export const openLink = (link: string): void => {
     window.open(link)
   }
 }
+
+const mobileRE = /(android|bb\d+|meego).+mobile|armv7l|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i
+
+const tabletRE = /android|ipad|playbook|silk/i
+
+export const isMobileDevice = (opts?: any): boolean => {
+  if (opts == null) opts = {}
+  let ua = opts.ua
+  if (ua == null && typeof navigator !== 'undefined') ua = navigator.userAgent
+  if (ua?.headers != null && typeof ua.headers['user-agent'] === 'string') {
+    ua = ua.headers['user-agent']
+  }
+  if (typeof ua !== 'string') return false
+
+  let result = mobileRE.test(ua) || (opts?.tablet != null && tabletRE.test(ua))
+
+  if (
+    !result &&
+    opts?.tablet != null &&
+    opts?.featureDetect != null &&
+    navigator?.maxTouchPoints > 1 &&
+    ua.includes('Macintosh') &&
+    ua.includes('Safari')
+  ) {
+    result = true
+  }
+
+  return result
+}
+
+export const isMobile = isMobileDevice()
